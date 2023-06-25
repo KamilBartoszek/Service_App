b521bbee6431a07681aa5dbd2529797e40136068
Brux <bruki28@gmail.com>
2023-06-25 18:07:58 +0200

    Drobne zmiany w ServiceOrdeersModels

--- ServiceApp/Models/ServiceOrders.cs
+++ ServiceApp/Models/ServiceOrders.cs
@@ -6,11 +6,12 @@ namespace ServiceApp.Models
     {
         public int Id { get; set; }
         public int OrderID { get; set; }
-        [DataType(DataType.DateTime)]
-        public DateTime OrderDate { get; set; } = DateTime.Now;
-        public string SerialNumber { get; set; }
-        public string Type { get; set; }
-        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; } =DateTime.Now;
+        public string? SerialNumber { get; set; }
+
+        public string? Type { get; set; }
+        public string? Description { get; set; }
         public bool PowerAdapter { get; set; }
         public bool IsFixed { get; set; }
     }
